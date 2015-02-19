fd6953da4772774e51abf3e60a943f648ddf5c80
Arthur Jen <cowboy5110@hotmail.com>
2015-02-19 04:05:21 -0800

    Added server and lock to the custom Flask class

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
 from app import app
+from threading import Semaphore
+from resources.server import Server
 
-app.run(debug=True)
+if __name__ == '__main__':
+    app_server = Server()
+    lock = Semaphore()
+    app.run(app_server, lock, debug=True)
